a9482d34222bb6fc533926a006a89478c300db90
jaysc <9077125+jaysc@users.noreply.github.com>
2025-01-25 15:55:23 +0900

    Use character assets

--- entities/player.gd
+++ entities/player.gd
@@ -2,10 +2,10 @@ extends CharacterBody2D
 
 var MAX_SPEED = 400
 var DASH_SPEED = 600    
-var ACCELERATION = 2000
-var DECELERATION = 2000
+var ACCELERATION = 200000
+var DECELERATION = 200000
 var dash_time = 0.0
-var DASH_DURATION = 0.3
+var DASH_DURATION = 0.1
 
 
 func _physics_process(delta: float) -> void:
@@ -13,12 +13,10 @@ func _physics_process(delta: float) -> void:
 	var direction = input_vector.normalized()
 
 	if Input.is_action_just_pressed("ui_select"):
-		
 		dash_time = DASH_DURATION
 
 	if dash_time > 0:
 		dash_time -= delta
-		
 
 	var target_speed = MAX_SPEED
 	if dash_time > 0:
