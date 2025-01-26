b272d6f99b3b3e27fe6097ff4f8b15efba031094
huylq2603 <huylq2603@gmail.com>
2025-01-26 16:34:44 +0900

    add arena blowing

---
+++ entities/top_blow_down.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _process(delta: float) -> void:
+	var areas = get_overlapping_areas()
+	if areas:
+		for area in areas:
+			var bubble_size = area.get_parent().get("SIZE")
+			if bubble_size:
+				var bubble = area.get_parent();
+				bubble.handle_rebound(Vector2.DOWN * 0.5)
+					
