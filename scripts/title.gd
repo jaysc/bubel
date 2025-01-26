fca2c488725d60d8da90e5b2d2cbd0ebf47fef81
jaysc <9077125+jaysc@users.noreply.github.com>
2025-01-26 15:24:33 +0900

    update scene change logic

--- scripts/title.gd
+++ scripts/title.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var nextScene = preload("res://scenes/main.tscn").instantiate()
+var nextScene = preload("res://scenes/main.tscn")
 
 
 func _on_button_button_down() -> void:
-	get_tree().root.add_child(nextScene)
+	get_tree().change_scene_to_packed(nextScene)
