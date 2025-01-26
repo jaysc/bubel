65be8da8a00678d36b9aeb4f461b5505fbd88b2c
jaysc <9077125+jaysc@users.noreply.github.com>
2025-01-26 10:26:15 +0900

    bubble shrink on hit

--- scripts/bubble_object.gd
+++ scripts/bubble_object.gd
@@ -18,5 +18,7 @@ func _on_area_entered(area: Area2D) -> void:
 			print('bubble hit size')
 			get_parent().queue_free()
 		else:
+			print('bubble hit shrink')
+			get_parent().createBubbleObject(get_parent().get("SIZE") - target_bubble_size)
 			pass
 			#handle shrink
