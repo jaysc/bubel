87700ae847500c153cc74be53e5dd9c21ff73143
jaysc <9077125+jaysc@users.noreply.github.com>
2025-01-26 11:14:38 +0900

    add barrel bubble pop and health

--- barrel.gd
+++ barrel.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+@export var health: float = 100;
+@export var is_destroyed := false;
+
 var looped_amount = 0
 const MAX_LOOP_AMOUNT = 3
 
-func on_hit() -> void:
-	$AnimatedSprite2D.speed_scale = 1
-	$AnimatedSprite2D.play()
+func on_hit(damage: float) -> void:
+	if !is_destroyed:
+		health -= damage
+		$AnimatedSprite2D.speed_scale = 1
+		$AnimatedSprite2D.play()
+		
+		if health <= 0:
+			destory_barrel()
 
 func _on_animated_sprite_2d_animation_looped() -> void:
 	if looped_amount >= MAX_LOOP_AMOUNT:
@@ -15,3 +23,14 @@ func _on_animated_sprite_2d_animation_looped() -> void:
 		looped_amount = 0
 
 	looped_amount += 1
+	
+func destory_barrel() -> void:
+	is_destroyed = true
+	pass
+
+
+func _on_area_entered(area: Area2D) -> void:
+	var target_bubble_size = area.get_parent().get('SIZE')
+	if target_bubble_size:
+		on_hit(target_bubble_size)
+		area.get_parent().destroyBubble()
