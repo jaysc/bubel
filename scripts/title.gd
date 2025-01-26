1e6a49166b8e2e425fe785a377e315f69322739d
jaysc <9077125+jaysc@users.noreply.github.com>
2025-01-26 15:35:50 +0900

    added  title

--- scripts/title.gd
+++ scripts/title.gd
@@ -2,6 +2,6 @@ extends Node2D
 
 var nextScene = preload("res://scenes/main.tscn")
 
-
-func _on_button_button_down() -> void:
-	get_tree().change_scene_to_packed(nextScene)
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_packed(nextScene)
