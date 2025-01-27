60d7ddbe9c0edfd5ea47bd5626604f9c743ae599
jaysc <9077125+jaysc@users.noreply.github.com>
2025-01-27 14:01:42 +0900

    integer health

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -36,9 +36,9 @@ func _process(delta: float) -> void:
 
 func _on_player_1_barrel_group_barrel_group_hit(health: float) -> void:
 	Player_1_Goal_Health = health
-	$"MarginContainer/VBoxContainer/Player 1 Health/Player 1 Health".text = str(Player_1_Goal_Health)
+	$"MarginContainer/VBoxContainer/Player 1 Health/Player 1 Health".text = str(int(Player_1_Goal_Health))
 
 
 func _on_player_2_barrel_group_barrel_group_hit(health: float) -> void:
 	Player_2_Goal_Health = health
-	$"MarginContainer2/VBoxContainer2/HBoxContainer2/Player 2 Health".text = str(Player_2_Goal_Health)
+	$"MarginContainer2/VBoxContainer2/HBoxContainer2/Player 2 Health".text = str(int(Player_2_Goal_Health))
